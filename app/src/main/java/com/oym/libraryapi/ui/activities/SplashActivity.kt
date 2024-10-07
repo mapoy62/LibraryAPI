109aece8f140968afa043491f06d3220e0163f5b
oyuyump <yuyumpoptec@gmail.com>
2024-10-06 22:41:41 -0600

    Fix Splash screen with coroutines

--- app/src/main/java/com/oym/libraryapi/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/oym/libraryapi/ui/activities/SplashActivity.kt
@@ -2,28 +2,35 @@ package com.oym.libraryapi.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.oym.libraryapi.R
+import com.oym.libraryapi.databinding.ActivityMainBinding
+import com.oym.libraryapi.databinding.ActivitySplashBinding
 import com.oym.libraryapi.ui.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        val splashScreen = installSplashScreen()
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        splashScreen.setOnExitAnimationListener{ splashScreenView ->
-            splashScreenView.remove()
-
-            //Iniciando la actividad principal
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000) // Espera 2 segundos
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish() // Cierra la actividad de splash
         }
 
     }
